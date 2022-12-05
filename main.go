6f49fe419c0ba35fe1da98a790032e1fc2339a74
Sebastian Berge <sebastianbrg@gmail.com>
2022-12-05 20:57:19 +0000

    add interaction with GPT model

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"github.com/openai/gpt-3-go/gpt3"
 )
 func main() {
 	// lets start to play. I have no idea where this will end. 20 minutes. Go!
@@ -16,7 +17,20 @@ tokens := tokenizeText(text)
 
 // Print the tokens.
 fmt.Println(tokens) // ["This", "is", "an", "example", "of", "tokenized", "text."]
+	key:= os.GetEnv("GPT_KEY")
+// Create a new GPT-3 client
+client := gpt3.NewClient(key)
 
+// Generate text using the pre-trained GPT-3 model
+response, err := client.Generate(gpt3.GenerateOpts{
+	Prompt: "Hello world!",
+	Length: 100,
+})
+if err != nil {
+	fmt.Println(err)
+} else {
+	fmt.Println(response.Data.Choices[0].Text)
+}
 
 
 }
